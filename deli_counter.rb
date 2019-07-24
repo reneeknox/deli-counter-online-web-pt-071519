236d22d32c110ea8e9fc0f17c7e01b2e0e39104a
Renee France <renee5.0@outlook.com>
2019-07-24 21:10:11 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -20,3 +20,11 @@ def take_a_number(katz_deli, name)
   katz_deli.push(name)
   puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end
+def now_serving(nextinline)
+  if nextinline.empty?
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{nextinline[0]}."
+    nextinline.shift
+  end
+end
